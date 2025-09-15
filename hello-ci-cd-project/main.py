e58912ebaa4754fb2fad82955a99c81527977750
duncanrtennant <duncanrtennant@gmail.com>
2025-09-15 16:15:19 -0500

    Attempted project fix

---
+++ hello-ci-cd-project/main.py
@@ -0,0 +1,11 @@
+# git commit -m 'commit message'
+# git push -u origin main
+def main():
+    print("Hello from hello-ci-cd!")
+
+def addTwo(x,y):
+    return (x+y) 
+
+if __name__ == "__main__":
+    main()
+
---
+++ hello-ci-cd-project/test_main.py
@@ -0,0 +1,7 @@
+from main import addTwo
+
+def test_addTwo():
+    assert(addTwo(12,5)==17)
+
+def test_addTwo2():
+    assert(addTwo(-1,-1)==-2)
