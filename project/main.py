1eaa91fd909d4a2501fda85c3891d888fde83f8e
duncanrtennant <duncanrtennant@gmail.com>
2026-01-12 00:03:02 -0600

    Updated structure and added tests

---
+++ project/main.py
@@ -0,0 +1,5 @@
+def isEven(int):
+    if(int%2==0):
+        return True
+    return False
+    
---
+++ tests/tests.py
@@ -0,0 +1,8 @@
+import pytest
+
+from project.main import isEven
+
+def test_isEven(int):
+    assert isEven(4) == True
+    assert isEven(3) == False
+    assert isEven(0) == True
