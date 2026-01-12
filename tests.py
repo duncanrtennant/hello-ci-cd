315e33c1bb86c25753956252b58eb0c3d181bf37
duncanrtennant <duncanrtennant@gmail.com>
2026-01-12 00:44:10 -0600

    tests.py updated

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import pytest
 
 from project.main import isEven
 
-def test_isEven(int):
+def test_isEven():
     assert isEven(4) == True
     assert isEven(3) == False
     assert isEven(0) == True
